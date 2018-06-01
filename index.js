c4bc598082becf61879ce4897f140490166c2906
Peter Tran <tran.peter08@gmail.com>
2018-06-01 14:50:15 -0700

    updated js HTML

--- index.js
+++ index.js
@@ -1,53 +1,42 @@
 const ZOMATO_KEY = 'abda2f58116a5e1ef63ea8bbb843f6da'
 
 
-/* const REQUEST_URL = "https://app.ticketmaster.com/discovery/v2/events.json?size=5&apikey=z1yWDATxAuenxouu5ujPMPXc8QyHgyh8"
-
-function getDataAPI(str,callback){
-  const settings = {
-    type: 'GET',
-    data:{
-      keyword: str,
-      countryCode: 'US',
-      sort:'date,asc'
-    },
-    async:true,
-    dataType: 'json',
-    success: callback
-  }
-  $.ajax(REQUEST_URL,settings)
-}
-
-
-function showResults(result){
-  console.log(result._embedded.events);
-}
-
-getDataAPI('hollywood ca',showResults); */
-
-
-function renderHTMLEvents(){
-   
-}
-
-function displayDataEvents(){
+function displayTicketmasterData(results){
+    console.log(results);
 
 }
 
-//get API data from Zomato 
-function getDataEvents(){
-
+//get API data from Ticket Master 
+function getTicketmasterData(aLocation, callBack){
+    const requestURL = 'https://app.ticketmaster.com/discovery/v2/events.json?size=5&apikey=z1yWDATxAuenxouu5ujPMPXc8QyHgyh8'
+    const settings = {
+        data:{
+            keyword: aLocation,
+            countryCode: 'US',
+            sort: 'date,asc',
+            size: 5
+        },
+        type: 'GET',
+        async:true,
+        dataType: 'json',
+        success:callBack
+        
+    };
+    $.ajax(requestURL, settings);
 }
 
 function renderHTMLRestaurants(item){
-    console.log(item)
     const {cuisines, featured_image, location,name, url, user_rating} = item.restaurant;
     return `
-        <h4><a href=${url} target='_blank'>${name}</a></h4>
-        <img src="${featured_image}" alt="Picture of restaurant">
-        <p>Cuisine: ${cuisines}</p>
-        <p>Customer Rating: ${user_rating.aggregate_rating}</p>
-        <address>${location.address}</address>
+        <ul>
+            <li>
+                <h4><a href=${url} target='_blank'>${name}</a></h4>
+                <img src="${featured_image}" alt="Picture of restaurant">
+                <p>Cuisine: ${cuisines}</p>
+                <p>Customer Rating: ${user_rating.aggregate_rating}</p>
+                <address>${location.address}</address>
+            </li>
+        </ul>
     `
 }
 
@@ -62,8 +51,8 @@ function displayZomatoData(results){
     `);
     $('.js-restaurants').html(`
         <div>
-        <h3>Restuarants</h3>
-        ${best_rated_restaurant.map((item, index) => renderHTMLRestaurants(item)).join('')}
+            <h3>Restuarants</h3>
+            ${best_rated_restaurant.map((item, index) => renderHTMLRestaurants(item)).join('')}
         <div>
         `)
 }
@@ -109,19 +98,14 @@ function getZomatoDataInit(aLocation, callBack){
     $.ajax(initialRequestURL,settings);
 }
 
-function renderHTMLWeather(){
-   //not thing yet
-}
-
 function displayDataWeather(results){
-    const {temp, weather} = results.data[0]
-    console.log(results);
+    const {temp, weather} = results.data[0];
     $('.js-weather').html(`
         <div>
-        <h3 class="js-weather-header">Weather</h3>
-        <img src="#" alt="Icon repesention of the current weather">
-        <p>Current Temperature: ${temp}</p>
-        <p>Description: ${weather.description}</p>
+            <h3 class="js-weather-header">Weather</h3>
+            <img src="#" alt="Icon repesention of the current weather">
+            <p>Current Temperature: ${temp}</p>
+            <p>Description: ${weather.description}</p>
         </div>
     `)
 }
@@ -147,7 +131,8 @@ function submitButtonClick(){
         let state = $('#state').val();
         let location = `${city} ${state}`
         getWeatherData(city, state, displayDataWeather);
-        getZomatoDataInit(location,handleInitZomatoData)
+        getZomatoDataInit(location,handleInitZomatoData);
+        getTicketmasterData(location,displayTicketmasterData);
         $('#city').val("");
         $('#state').val("");
     })
