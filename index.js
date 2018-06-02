c1b2f141a85e33b69c404e40f0caf22647388da5
Peter Tran <tran.peter08@gmail.com>
2018-06-02 03:37:26 -0700

    updated js

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 const ZOMATO_KEY = 'abda2f58116a5e1ef63ea8bbb843f6da'
 
+function renderDateTime(aTime){
+    let date = new Date(aTime);
+    const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    const weekDays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let weekDay = weekDays[date.getDay()];
+    let month = months[date.getMonth()];
+    let day = date.getDate();
+    let year = date.getFullYear();
+    let milTime = date.getHours();
+    let timePeriod = 'AM';
+    let hour;
+    if(milTime > 11){
+      hour = milTime-12;
+      timePeriod = 'PM'
+    }
+    else{
+        hour = milTime;
+    }
+    if(hour == 0){
+      hour==12
+    }
+    let minute = date.getMinutes();
+    minute = (minute <10)? minute + '0': minute;
+    return `${weekDay} ${month} ${day}, ${year} @ ${hour}:${minute} ${timePeriod}`
+}
+
+function renderEvents(item){
+    const {name, dates, _embedded, images, url} = item;
+    const {address, city, postalCode, state} = item._embedded.venues[0]
+    return `
+        <ul>
+            <li>
+                <h4><a href='${url}' target='_blank'>${name}</a></h4>
+                <img src="${images[0].url}" alt="event picture">
+                <h5>${_embedded.venues[0].name}</h5>
+                <p>Date and time: ${renderDateTime(dates.start.dateTime)}</p>
+                <address>${address.line1}, ${city.name}, ${state.stateCode} ${postalCode}</address>
+            </li>
+        </ul>
+    `
+}
 
 function displayTicketmasterData(results){
     console.log(results);
-
+    $('.js-events').html(`
+        <div class=''>
+            <h3>Events</h3>
+            ${results._embedded.events.map((item,index)=> renderEvents(item)).join('')}
+        </div>
+    `)
 }
 
 //get API data from Ticket Master 
@@ -13,19 +59,16 @@ function getTicketmasterData(aLocation, callBack){
         data:{
             keyword: aLocation,
             countryCode: 'US',
-            sort: 'date,asc',
-            size: 5
         },
         type: 'GET',
         async:true,
         dataType: 'json',
         success:callBack
-        
     };
     $.ajax(requestURL, settings);
 }
 
-function renderHTMLRestaurants(item){
+function renderRestaurants(item){
     const {cuisines, featured_image, location,name, url, user_rating} = item.restaurant;
     return `
         <ul>
@@ -52,9 +95,9 @@ function displayZomatoData(results){
     $('.js-restaurants').html(`
         <div>
             <h3>Restuarants</h3>
-            ${best_rated_restaurant.map((item, index) => renderHTMLRestaurants(item)).join('')}
+            ${best_rated_restaurant.map((item, index) => renderRestaurants(item)).join('')}
         <div>
-        `)
+    `)
 }
 
 function getZomatoDataDetail(anID,aType,callBack){
@@ -98,6 +141,7 @@ function getZomatoDataInit(aLocation, callBack){
     $.ajax(initialRequestURL,settings);
 }
 
+//get API data from WeatherIO
 function displayDataWeather(results){
     const {temp, weather} = results.data[0];
     $('.js-weather').html(`
