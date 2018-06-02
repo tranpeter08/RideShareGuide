34e9068ed06c874f99fc3f77fe86c76fdd266fdd
Peter Tran <tran.peter08@gmail.com>
2018-06-02 16:36:30 -0700

    updated js and html

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function renderDateTime(aTime){
     else{
         hour = milTime;
     }
-    if(hour == 0){
+    if(hour === 0){
       hour==12
     }
     let minute = date.getMinutes();
@@ -27,6 +27,7 @@ function renderDateTime(aTime){
 }
 
 function renderEvents(item){
+
     const {name, dates, _embedded, images, url} = item;
     const {address, city, postalCode, state} = item._embedded.venues[0]
     return `
@@ -42,14 +43,26 @@ function renderEvents(item){
     `
 }
 
+function noTicketmaster(){
+    $('.js-events').html(`
+        <div class=''>
+            <h3>Events</h3>
+            <p>No event information found</p>
+        </div>
+    `)
+}
+
 function displayTicketmasterData(results){
-    console.log(results);
+    if(results._embedded == undefined){
+        noTicketmaster();
+    }else{
     $('.js-events').html(`
         <div class=''>
             <h3>Events</h3>
             ${results._embedded.events.map((item,index)=> renderEvents(item)).join('')}
         </div>
     `)
+    }
 }
 
 //get API data from Ticket Master 
@@ -87,7 +100,7 @@ function displayZomatoData(results){
     const {location, popularity, nightlife_index, best_rated_restaurant} = results;
     $('.js-city-info').html(`
         <div>
-            <h3>City stats for: ${location.title}</h3>
+            <h3>City stats for ${location.title}</h3>
             <p>Nightlife score of: ${nightlife_index}</p>
             <p>Popularity score: ${popularity}</p>
         </div>
@@ -118,9 +131,29 @@ function getZomatoDataDetail(anID,aType,callBack){
     $.ajax(secondRequestURL,settings);
 }
 
+function handleNoZomato(){
+    $('.js-city-info').html(`
+        <div>
+            <h3>City info</h3>
+            <p>No city info found<p>
+        </div>
+    `);
+    $('.js-restaurants').html(`
+    <div>
+        <h3>Restuarants</h3>
+        <p>No restaurant information found<p>
+    <div>
+    `)
+}
+
 function handleInitZomatoData(initResults){
-    const {entity_id, entity_type}= initResults.location_suggestions[0];
-    getZomatoDataDetail(entity_id, entity_type, displayZomatoData);
+    if(initResults.location_suggestions.length == 0){ 
+        handleNoZomato()
+    }
+    else{
+        const {entity_id, entity_type}= initResults.location_suggestions[0];
+        getZomatoDataDetail(entity_id, entity_type, displayZomatoData);
+    }
 }
 
 //get API data from Zomato 
@@ -141,17 +174,31 @@ function getZomatoDataInit(aLocation, callBack){
     $.ajax(initialRequestURL,settings);
 }
 
-//get API data from WeatherIO
-function displayDataWeather(results){
-    const {temp, weather} = results.data[0];
+function noWeather(){
     $('.js-weather').html(`
         <div>
             <h3 class="js-weather-header">Weather</h3>
-            <img src="#" alt="Icon repesention of the current weather">
+            <p>No weather information found<p>
+        </div>
+    `)
+}
+
+//get API data from WeatherIO
+function displayDataWeather(results){
+    if(results == undefined){
+       noWeather();
+    }
+    else{
+    const {city_name, state_code, temp, weather} = results.data[0];
+    $('.js-weather').html(`
+        <div>
+            <h3 class="js-weather-header">Weather for ${city_name}, ${state_code}</h3>
+            <img src="https://www.weatherbit.io/static/img/icons/${weather.icon}.png" alt="Icon repesention of the current weather">
             <p>Current Temperature: ${temp}</p>
             <p>Description: ${weather.description}</p>
         </div>
     `)
+    }
 }
 
 //get API data from weather 
@@ -167,16 +214,31 @@ function getWeatherData(aCity,aState,callBack){
     $.getJSON(weatherURL, query, callBack);
 }
 
+function clearResults(){
+    $('.js-results').empty();
+    $('.js-weather').empty();
+    $('.js-city-info').empty();
+    $('.js-restaurants').empty();
+    $('.js-events').empty();
+}
+
+function renderResultHead(aCity, aState){
+    $('.js-results').html(`<h2>Results for "${aCity} ${aState}"</h2>`);
+}
+
 //user clicks submit, updates user input
 function submitButtonClick(){
     $('#js-form').on('submit', (event)=>{
         event.preventDefault();
         let city = $('#city').val();
         let state = $('#state').val();
-        let location = `${city} ${state}`
+        let location = `${city} ${state}`;
+        console.log(location);
+        clearResults();
         getWeatherData(city, state, displayDataWeather);
         getZomatoDataInit(location,handleInitZomatoData);
         getTicketmasterData(location,displayTicketmasterData);
+        renderResultHead(city,state);
         $('#city').val("");
         $('#state').val("");
     })
