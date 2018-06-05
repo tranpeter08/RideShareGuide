228db2630af548013be0e633de331636b28c1e03
Peter Tran <tran.peter08@gmail.com>
2018-06-05 03:28:12 -0700

    updated all files for design

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function showErr(err){
+    console.log(err);
+}
+
 function renderDateTime(aTime){
     let date = new Date(aTime);
     const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
@@ -18,7 +22,7 @@ function renderDateTime(aTime){
     else{
         hour = milTime;
     }
-    if(hour === 0){
+    if(hour == 0){
       hour==12
     }
     let minute = date.getMinutes();
@@ -30,21 +34,20 @@ function renderEvents(item){
     const {name, dates, _embedded, images, url} = item;
     const {address, city, postalCode, state} = item._embedded.venues[0]
     return `
-        <ul>
-            <li>
-                <h4><a href='${url}' target='_blank'>${name}</a></h4>
-                <img src="${images[0].url}" alt="event picture">
-                <h5>${_embedded.venues[0].name}</h5>
-                <p>Date and time: ${renderDateTime(dates.start.dateTime)}</p>
-                <address>${address.line1}, ${city.name}, ${state.stateCode} ${postalCode}</address>
-            </li>
-        </ul>
+        <hr>
+        <li>
+            <h4><a href='${url}' target='_blank'>${name}</a></h4>
+            <img src="${images[0].url}" alt="event picture">
+            <h5>${_embedded.venues[0].name}</h5>
+            <p>Date and time: ${renderDateTime(dates.start.dateTime)}</p>
+            <address>${address.line1}, ${city.name}, ${state.stateCode} ${postalCode}</address>
+        </li>
     `
 }
 
 function noTicketmaster(){
     $('.js-events').html(`
-        <div class='js-border'>
+        <div class='js-border js-height-big'>
             <h3>Events</h3>
             <p>No event information found</p>
         </div>
@@ -56,9 +59,11 @@ function displayTicketmasterData(results){
         noTicketmaster();
     }else{
     $('.js-events').html(`
-        <div class='js-border'>
+        <div class='js-border js-height-big'>
             <h3>Events</h3>
-            ${results._embedded.events.map((item,index)=> renderEvents(item)).join('')}
+            <ul>
+                ${results._embedded.events.map((item,index)=> renderEvents(item)).join('')}
+            </ul>
         </div>
     `)
     }
@@ -70,50 +75,53 @@ function getTicketmasterData(aLocation, callBack){
     const settings = {
         data:{
             keyword: aLocation,
-            countryCode: 'US',
+            countryCode:'US'
         },
         type: 'GET',
         async:true,
         dataType: 'json',
         success:callBack
     };
-    $.ajax(requestURL, settings);
+    $.ajax(requestURL, settings)
+    .fail(showErr);
 }
 
 function renderStockPhoto(featImage){
-   return (featImage == "")? 'https://cdn.pixabay.com/photo/2015/03/26/10/28/restaurant-691397_1280.jpg': featImage;
+   return (featImage == "")? 'https://i.imgur.com/63HPxXC.jpg': featImage;
 }
 
 function renderRestaurants(item, index){
     const {cuisines, featured_image, location,name, url, user_rating} = item.restaurant;
     if(index <5){
     return `
-        <ul>
-            <li>
-                <h4><a href=${url} target='_blank'>${name}</a></h4>
-                <img src="${renderStockPhoto(featured_image)}" alt="Picture of restaurant">
-                <p>Cuisine: ${cuisines}</p>
-                <p>Customer Rating: ${user_rating.aggregate_rating}</p>
-                <address>${location.address}</address>
-            </li>
-        </ul>
+        <hr>
+        <li>
+            <h4><a href=${url} target='_blank'>${name}</a></h4>
+            <img src="${renderStockPhoto(featured_image)}" alt="Picture of restaurant">
+            <p>Cuisine: ${cuisines}</p>
+            <p>Customer Rating: ${user_rating.aggregate_rating}/5</p>
+            <address>${location.address}</address>
+        </li>
     `
     }
 }
 
 function displayZomatoData(results){
+    console.log(results);
     const {location, popularity, nightlife_index, best_rated_restaurant} = results;
     $('.js-city-info').html(`
-        <div class='js-border'>
-            <h3>City stats for ${location.title}</h3>
-            <p>Nightlife score of: ${nightlife_index}</p>
-            <p>Popularity score: ${popularity}</p>
+        <div class='js-border js-height-small'>
+            <h3 class='city-stats-h3'>City stats for ${location.title}, ${location.country_name}</h3>
+            <p>Nightlife score: ${nightlife_index}/5</p>
+            <p>Popularity score: ${popularity}/5</p>
         </div>
     `);
     $('.js-restaurants').html(`
-        <div class='js-border'>
+        <div class='js-border js-height-big'>
             <h3>Restuarants</h3>
-            ${best_rated_restaurant.map((item, index) => renderRestaurants(item, index)).join('')}
+            <ul>
+                ${best_rated_restaurant.map((item, index) => renderRestaurants(item, index)).join('')}
+            </ul>
         <div>
     `)
 }
@@ -134,18 +142,19 @@ function getZomatoDataDetail(anID,aType,callBack){
         type:'GET',
         success: callBack
     };
-    $.ajax(secondRequestURL,settings);
+    $.ajax(secondRequestURL,settings)
+    .fail(showErr);
 }
 
 function handleNoZomato(){
     $('.js-city-info').html(`
-        <div class='js-border'>
+        <div class='js-border js-height-small'>
             <h3>City info</h3>
             <p>No city info found<p>
         </div>
     `);
     $('.js-restaurants').html(`
-    <div class='js-border'>
+    <div class='js-border js-height-big'>
         <h3>Restuarants</h3>
         <p>No restaurant information found<p>
     <div>
@@ -153,6 +162,7 @@ function handleNoZomato(){
 }
 
 function handleInitZomatoData(initResults){
+    console.log(initResults);
     if(initResults.location_suggestions.length == 0){ 
         handleNoZomato()
     }
@@ -178,12 +188,13 @@ function getZomatoDataInit(aLocation, callBack){
         type: 'GET',
         success: callBack,
     };
-    $.ajax(initialRequestURL,settings);
+    $.ajax(initialRequestURL,settings)
+    .fail(showErr);
 }
 
 function noWeather(){
     $('.js-weather').html(`
-        <div class='js-border'>
+        <div class='js-border js-height-small'>
             <h3 class="js-weather-header">Weather</h3>
             <p>No weather information found<p>
         </div>
@@ -192,66 +203,69 @@ function noWeather(){
 
 //get API data from WeatherIO
 function displayDataWeather(results){
+    console.log(results);
     if(results == undefined){
        noWeather();
     }
     else{
-    const {city_name, state_code, temp, weather} = results.data[0];
+    const {city_name, state_code, country_code, temp, weather} = results.data[0];
     $('.js-weather').html(`
-        <div class='js-border'>
-            <h3 class="js-weather-header">Weather for ${city_name}, ${state_code}</h3>
+        <div class='js-border js-height-small'>
+            <h3 class="js-weather-header">Weather for ${city_name}, ${state_code}, ${country_code}</h3>
             <img src="https://www.weatherbit.io/static/img/icons/${weather.icon}.png" alt="Icon repesention of the current weather" class="js-weather-img">
             <div class="js-weather-p">
-                <p>Current Temperature: ${temp}</p>
-                
+                <p>Current Temperature: ${temp}&degF</p>
                 <p>Description: ${weather.description}</p>
             </div>
         </div>
-    `)
+    `);
     }
 }
 
 //get API data from weather 
-function getWeatherData(aCity,aState,callBack){
+function getWeatherData(aCityState,callBack){
     const weatherURL = 'https://api.weatherbit.io/v2.0/current?'
     const query = {
         key:`2a0a26cd2e95437c9cd83e7a8e5d77b8`,
         units: 'I',
-        city: aCity,
-        state: aState,
+        city: aCityState,
         country: 'US'
     };
     $.getJSON(weatherURL, query, callBack);
 }
 
 function clearResults(){
-    $('.dislpay').empty();
+    $('.display').empty();
 }
 
-function renderResultHead(aCity, aState){
-    $('.js-results').html(`<h2 class=''>Results for "${aCity} ${aState}"</h2>`);
+function headerButton(){
+    $('.header-button').on('click', function(event){
+    clearResults();
+    })
+}
+
+function renderResultHead(aLocation){
+    $('.js-results').html(`<h2 class='js-scroll'>Results for "${aLocation.trim()}"</h2>`);
 }
 
 //user clicks submit, updates user input
 function submitButtonClick(){
     $('#js-form').on('submit', (event)=>{
         event.preventDefault();
-        let city = $('#city').val();
-        let state = $('#state').val();
-        let location = `${city} ${state}`;
-        console.log(location);
+        
         clearResults();
-        getWeatherData(city, state, displayDataWeather);
-        getZomatoDataInit(location,handleInitZomatoData);
-        getTicketmasterData(location,displayTicketmasterData);
-        renderResultHead(city,state);
-        $('#city').val("");
-        $('#state').val("");
+        let cityState = $('#city-state').val();
+        getWeatherData(cityState, displayDataWeather);
+        getZomatoDataInit(cityState,handleInitZomatoData);
+        getTicketmasterData(cityState,displayTicketmasterData);
+        renderResultHead(cityState);
+        $('input').val("");
     })
 }
 
 function runThis(){
-    submitButtonClick()
+    submitButtonClick();
+    headerButton();
 }
 
-$(runThis)
+$(runThis);
